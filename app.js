e7b66338facfa9c2acfa17512cd4039ebd5d80c2
Laurence Capitan <renzcap@codemagnus.com>
2016-12-14 18:42:54 +0800

    Invoke ANT with mocha

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello Node via Jenkins!!");
+  res.send("Hello Node via Jenkins with mocha!!");
 });
 
 app.listen(4000);
